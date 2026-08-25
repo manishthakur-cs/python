1c64fe17d3d330812fcd234c13ab0e4d8fbb21f4
manishthakur <manish.thakur.cs@gmail.com>
2026-08-26 02:02:48 +0530

    fixed game

--- project 2/main.py
+++ project 2/main.py
@@ -1,13 +1,15 @@
 import random 
 n = random.randint(1,100)
 a = -1
-guesses = 0
+guesses = 1
 while(a != n):
     guesses += 1
     a = int( input("guess the number : "))
     if(a >n):
         print("lower number please")
-    else:
+        guesses += 1
+        
+    elif(a < n):
         print("higher number please")
-    
+        guesses += 1
 print(f"you have gussed the number correctly {n} in {guesses} attempt")
